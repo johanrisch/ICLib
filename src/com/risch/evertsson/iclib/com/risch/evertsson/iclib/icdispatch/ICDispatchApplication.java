89add9384667c32f7d2202e998e8aca2f53e691e
jrisch <johan.risch@gmail.com>
2013-07-02 19:10:38 +0200

    hej

---
+++ src/com/risch/evertsson/iclib/com/risch/evertsson/iclib/icdispatch/ICDispatchApplication.java
@@ -0,0 +1,23 @@
+package com.risch.evertsson.iclib.com.risch.evertsson.iclib.icdispatch;
+
+import android.app.Application;
+
+/**
+ * Created by johanrisch on 6/21/13.
+ */
+public class ICDispatchApplication extends Application{
+    private static ICDispatch mICDispatch;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.mICDispatch = new ICDispatch();
+        this.mICDispatch.initICDispatch();
+    }
+    public static boolean executeOn(int queue, ICBlock block){
+        return mICDispatch.executeOn(queue,block);
+    }
+    public static boolean executeMethodOn(int queue,Object instance, String methodName, Object... args){
+        return mICDispatch.executeMethodOn(queue,instance,methodName,args);
+    }
+}
